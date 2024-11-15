7f55218106f1677c7e59ffe8154fe2abc1d7ee59
Ricky Smithson <rickyroysmithson@gmail.com>
2024-11-15 17:52:50 +0000

    add: generic post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-therickyroy-djangoblog-7kkbczaq0l8.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
